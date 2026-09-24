8590d9aef6bdb8a15ab79985a3893590be990b67
RaghavD1 <raghavdua.360@gmail.com>
2026-09-24 18:01:24 +0530

    Fix unused useEffect import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
